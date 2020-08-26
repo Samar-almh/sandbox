5ad33d528138d80424a58408cf889e50d46cfe02
Samar.almh <49631872+Samar-almh@users.noreply.github.com>
2020-08-26 12:03:36 +0300

    Initial commit

---
+++ src/TypeIntro.kt
@@ -0,0 +1,15 @@
+fun main(){
+
+    var experiencePoints:Int =5;
+    val playerName:String ="Estragon";
+
+    experiencePoints=experiencePoints+5
+    println(experiencePoints);
+    println(playerName);
+var hasSteed :Boolean=false;
+    if (hasSteed==false){
+        hasSteed=true;
+    }
+    var reflaction =playerName.reversed();
+    print(reflaction);
+}
